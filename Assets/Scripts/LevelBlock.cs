5c06431228780632694b3fb7fdecb98ddfb811e0
ChrisCam38 <kristiantilano@gmail.com>
2023-07-13 10:06:50 -0500

    Clase 25

---
+++ Assets/Scripts/LevelBlock.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelBlock : MonoBehaviour
+{
+
+    public Transform startPoint, exitPoint; //Declaracion de variables que refencian el punto de inicio y salida del LEVELBLOCK
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
